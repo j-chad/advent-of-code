5f3da4702431adbe07c65f076d044c0e38fd0420
Jackson <contact@jacksonc.dev>
2024-12-03 06:14:27 +1300

    almost day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -5,33 +5,10 @@ pub fn part_one(input: &str) -> Option<u32> {
     let reports = input.lines();
 
     for report in reports {
-        let mut is_safe: bool = true;
-        let mut is_ascending: Option<bool> = None;
-
         let levels = report.split_whitespace().map(str::parse::<u32>).collect::<Result<Vec<u32>, _>>().expect("invalid report");
-        for window in levels.windows(2) {
-            let [a, b] = window else { panic!("invalid window") };
+        let failed_index = check_report(&levels);
 
-            let ascending = a < b;
-            match is_ascending {
-                Some(is_ascending) => {
-                    if is_ascending != ascending {
-                        is_safe = false; // unsafe - data is fluctuating
-                        break;
-                    }
-                }
-                None => {
-                    is_ascending = Some(ascending);
-                }
-            }
-
-            let difference = if ascending { b - a } else { a - b };
-            if difference < 1 || difference > 3 {
-                is_safe = false;
-            }
-        }
-
-        if is_safe {
+        if failed_index.is_none() {
             safe += 1;
         }
     }
@@ -39,9 +16,61 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(safe)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut safe: u32 = 0;
+    let reports = input.lines();
+
+    for report in reports {
+        let mut levels = report.split_whitespace().map(str::parse::<u32>).collect::<Result<Vec<u32>, _>>().expect("invalid report");
+        let failed_index = check_report(&levels);
+
+        match failed_index {
+            Some(index) => {
+                levels.remove(index);
+                let failed_index = check_report(&levels);
+                if failed_index.is_none() {
+                    safe += 1;
+                }
+            }
+            None => {
+                safe += 1;
+                continue;
+            }
+        }
+    }
+
+    Some(safe)
+}
+
+// If the report fails - returns the index of the first failed level
+fn check_report(levels: &Vec<u32>) -> Option<usize> {
+    let mut is_ascending: Option<bool> = None;
+
+    for (index, window) in levels.windows(2).enumerate() {
+        let [a, b] = *window else { panic!("invalid window") };
+
+        let ascending = a < b;
+        match is_ascending {
+            Some(is_ascending) => {
+                if is_ascending != ascending {
+                    return Some(index); // unsafe - data is fluctuating
+                }
+            }
+            None => {
+                is_ascending = Some(ascending);
+            }
+        }
+
+        let difference = if ascending { b - a } else { a - b };
+        if difference < 1 || difference > 3 {
+            return Some(index); // unsafe - data changes too much or too little
+        }
+    }
+
     None
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -56,6 +85,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
