34ce4a04c14174c5bc1a58e8dbfc631aca814472
Jackson <contact@jacksonc.dev>
2024-12-03 20:25:22 +1300

    improve part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,16 +1,19 @@
-use regex::Regex;
+use regex::{Regex};
 
 advent_of_code::solution!(3);
 
+static MUL_TOKEN: &str = r"mul\((\d+),(\d+)\)";
+const ALL_TOKENS: &str = r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)";
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let mul_regex: Regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mul_regex: Regex = Regex::new(MUL_TOKEN).unwrap();
 
     let mut total: u32 = 0;
 
     // search input for all instances of "mul(a, b)"
     for cap in mul_regex.captures_iter(input) {
-        let a = cap[1].parse::<u32>().unwrap();
-        let b = cap[2].parse::<u32>().unwrap();
+        let a = cap[1].parse::<u32>().expect("Failed to parse integer");
+        let b = cap[2].parse::<u32>().expect("Failed to parse integer");
         total += a * b;
     }
 
@@ -18,34 +21,23 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let mul_regex: Regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let do_regex: Regex = Regex::new(r"do\(\)").unwrap();
-    let dont_regex: Regex = Regex::new(r"don't\(\)").unwrap();
+    let token_regex: Regex = Regex::new(ALL_TOKENS).unwrap();
 
     let mut total: u32 = 0;
 
-    let dont_matches: Vec<_> = dont_regex.find_iter(input).collect();
-    let do_matches: Vec<_> = do_regex.find_iter(input).collect();
-    let mul_matches: Vec<_> = mul_regex.find_iter(input).collect();
-
-    // collect all into a single vector
-    let mut all_matches: Vec<_> = dont_matches.iter().chain(do_matches.iter()).chain(mul_matches.iter()).collect();
-
-    // sort by start position
-    all_matches.sort_by_key(|m| m.start());
-
     // iterate over all matches
     let mut is_multiplying = true;
-    for m in all_matches {
-        if mul_matches.contains(&m) && is_multiplying {
-            let cap = mul_regex.captures(m.as_str()).unwrap();
-            let a = cap[1].parse::<u32>().unwrap();
-            let b = cap[2].parse::<u32>().unwrap();
-            total += a * b;
-        } else if dont_regex.is_match(m.as_str()) {
-            is_multiplying = false;
-        } else if do_regex.is_match(m.as_str()) {
+    for cap in token_regex.captures_iter(input) {
+        let token = &cap[0];
+
+        if token == "do()" {
             is_multiplying = true;
+        } else if token == "don't()" {
+            is_multiplying = false;
+        } else if is_multiplying {
+            let a = cap[1].parse::<u32>().expect("Failed to parse integer");
+            let b = cap[2].parse::<u32>().expect("Failed to parse integer");
+            total += a * b;
         }
     }
 
